ac5abf029f63d95ba29d1df62b90bf4bd7935df3
hirotana55555-max <hirotana55555@gmail.com>
2025-11-11 19:36:21 +0900

    refactor(Position): convert to TypeScript with readonly props

--- game/components/Position.js
+++
@@ -1,7 +0,0 @@
-// game/components/Position.js 【このコードで全文を置き換えてください】
-export class Position {
-  constructor({ x = 0, y = 0 } = {}) {
-    this.x = x;
-    this.y = y;
-  }
-}
--- game/components/index.js
+++ game/components/index.js
@@ -1,14 +1,13 @@
 // game/components/index.js 
-export * from './Controllable.js';
-export * from './InputState.js';
-export * from './Position.js';
-export * from './Renderable.js';
-export * from './Rotation.js';
-export * from './Velocity.js';
-export * from './Bullet.js';
-export * from './Team.js';
-export * from './Lifetime.js';
-export * from './Health.js';
-export * from './Collidable.js';
-export * from './Generator.js';
-
+export * from './Controllable';
+export * from './InputState';
+export * from './Position';
+export * from './Renderable';
+export * from './Rotation';
+export * from './Velocity';
+export * from './Bullet';
+export * from './Team';
+export * from './Lifetime';
+export * from './Health';
+export * from './Collidable';
+export * from './Generator';
