9481135cf4f773dd735916d698e69e029f8d3b41
hirotana55555-max <hirotana55555@gmail.com>
2025-11-11 17:09:37 +0900

    feat: Start refactoring Position.js component

---
+++ Project_Cognize/setup_env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update && sudo apt install -y nodejs npm git sqlite3 && npm install -g npm@latest && npm install --save @babel/parser@7.23.0 @babel/traverse@7.23.0 better-sqlite3@9.2.0 glob@10.3.10
